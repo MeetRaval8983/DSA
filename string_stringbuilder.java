4fa83ff475e4c8b467bd6d9578a3a8aeac365c06
MeetRaval8983 <meetraval2403@gmail.com>
2025-06-24 13:15:43 +0530

    String and StringBuilder

--- string_stringbuilder.java
+++ string_stringbuilder.java
@@ -44,5 +44,46 @@ public class string_stringbuilder {
         System.out.println("Hello" + new ArrayList<>());
         System.out.println("Hello " + new Integer(70));
         System.out.println(new Integer(70) + "" + new ArrayList<>());
+
+        s = "";
+        for(i = 0; i < 26; i++) {
+            char ch = (char)('a' + i);
+            s += ch;
+        }
+        System.out.println(s);
+        
+        // StringBuilder
+        StringBuilder sb =new StringBuilder();
+        for(i = 0; i < 26; i++) {
+            char ch = (char)('a' + i);
+            sb.append(ch);
+        }
+        System.out.println(sb.toString());
+        System.out.println(sb.deleteCharAt(0));
+        System.out.println(sb.reverse());
+
+        s = "Hello World";
+        System.out.println(s.toLowerCase());
+        System.out.println("     Welcome to Java     ".trim());
+        System.out.println(Arrays.toString(s.split(" ")));
+        System.out.println(s.replace("World", "Java"));
+
+        s = "abcba";
+        System.out.println(isPalindrome(s));
+
+        String str = "abc";
+        StringBuilder builder = new StringBuilder(str);
+        StringBuilder builder2 = new StringBuilder(str).reverse();
+        System.out.println(builder.toString().equals(builder2.toString()));
+    }
+    static boolean isPalindrome(String s) {
+        for(int i = 0; i <= s.length() / 2; i++) {
+            char start = s.charAt(i);
+            char end = s.charAt(s.length() - i - 1);
+            if(start != end) {
+                return false;
+            }
+        }
+        return true;
     }
 }
