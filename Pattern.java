81367e8d45f24be8903198c59388bd2c9d409f1e
MeetRaval8983 <meetraval2403@gmail.com>
2025-07-07 23:34:25 +0530

    Patter35

--- Pattern.java
+++ Pattern.java
@@ -576,5 +576,47 @@ public class Pattern {
         }
         System.out.println();
 
+        // Pattern 33
+        n = 5;
+        char c = 'a';
+        for(int i = 1; i <= n; i++) {
+            for(int j = 1; j <= i; j++) {
+                if((c - 'a') % 2 == 0) {
+                    System.out.print(Character.toLowerCase(c) + " ");
+                } else {
+                    System.out.print(Character.toUpperCase(c) + " ");
+                }
+                c++;
+            }
+            System.out.println();
+        }
+        System.out.println();
+
+        // Pattern 34
+        n = 5;
+        for(int i = 1; i <= n; i++){
+            int ch = 'A' + n - i;
+            for(int j = 1; j <= n - i + 1; j++) {
+                System.out.print((char)ch + " ");
+                ch--;
+            }
+            System.out.println();
+        }
+        System.out.println();
+
+        // Pattern 35
+        n = 4;
+        for(int i = 1; i <= n; i++) {
+            for(int j = 1; j <= i; j++) {
+                System.out.print(j);
+            }
+            for(int j = 1; j <= 2 * (n - i); j++) {
+                System.out.print(" ");
+            }
+            for(int j = i; j >= 1; j--) {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
     }
 }
