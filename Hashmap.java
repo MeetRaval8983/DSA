08dc2b113c19356a7340219095a2d85f995a237a
MeetRaval8983 <meetraval2403@gmail.com>
2025-07-17 00:09:19 +0530

    Hashmap

---
+++ Hashmap.java
@@ -0,0 +1,10 @@
+public class Hashmap {
+    public static void main(String[] args) {
+        System.out.println();
+        String s = "Meet";
+        int code = s.hashCode();
+        System.out.println("Hashcode: " + code);
+        Integer a = 123455;
+        System.out.println(a + " : " + a.hashCode());
+    }
+}
